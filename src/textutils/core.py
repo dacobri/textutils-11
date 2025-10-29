561096cbad805cf976611e7a4142cc6621bff86b
Brice Da Costa <ecirb1@gmail.com>
2025-10-29 16:39:23 +0100

    feat(is_anagram): Added an anagram check function

---
+++ src/textutils/core.py
@@ -0,0 +1,7 @@
+def is_anagram(text1, text2):
+    # Remove spaces and make lowercase
+    text1 = text1.replace(" ", "").lower()
+    text2 = text2.replace(" ", "").lower()
+    
+    # Sort the letters and compare
+    return sorted(text1) == sorted(text2)
