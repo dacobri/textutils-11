f4e4cd4c4478bceb4d203543acd24ad049ed838d
Brice Da Costa <ecirb1@gmail.com>
2025-10-29 17:13:36 +0100

    Adding anagram function

--- src/textutils/core.py
+++ src/textutils/core.py
@@ -1,8 +1,9 @@
 def is_anagram(text1, text2):
     #Checking for strings
-    if type(text) != str:
+    if type(text1) != str:
         raise TypeError("Input must be a string")
-    
+    if type(text2) != str:
+        raise TypeError("Input must be a string")
     # Remove spaces and make lowercase
     text1 = text1.replace(" ", "").lower()
     text2 = text2.replace(" ", "").lower()
