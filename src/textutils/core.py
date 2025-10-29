7ef72f3c779afa42f38227d3dbb31cd31198d7d1
Caroline Wheeler <carolinesofia.wheeler@alumni.esade.edu>
2025-10-29 17:10:39 +0100

    Adding string checker to Brice's is_anagram

--- src/textutils/core.py
+++ src/textutils/core.py
@@ -1,4 +1,8 @@
 def is_anagram(text1, text2):
+    #Checking for strings
+    if type(text) != str:
+        raise TypeError("Input must be a string")
+    
     # Remove spaces and make lowercase
     text1 = text1.replace(" ", "").lower()
     text2 = text2.replace(" ", "").lower()
